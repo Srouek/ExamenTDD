dc9e9a470c3a8c2f0598f196fdb0ff29ed83e4e1
Stephane ROUEK <srouek@gmail.com>
2021-09-22 15:10:51 +0200

    Simple Version work

--- src/DecimalToRoman.php
+++ src/DecimalToRoman.php
@@ -4,8 +4,17 @@ class DecimalToRoman{
 
     public static function decimalToRoman(int $number):string{
 
-        
-        return "";
+        if($number == 0) return "";
+        if($number == 1) return "I";
+        if($number == 2) return "II";
+        if($number == 3) return "III";
+        if($number == 4) return "IV";
+        if($number == 5) return "V";
+        if($number == 6) return "VI";
+        if($number == 7) return "VII";
+        if($number == 8) return "VIII";
+        if($number == 9) return "IX";
+        if($number == 10) return "X";
     }
 
 }
