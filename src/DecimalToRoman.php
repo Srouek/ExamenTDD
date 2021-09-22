1d3f0b48fbee1a9dc748c4c370df548c1633b538
Stephane ROUEK <srouek@gmail.com>
2021-09-22 16:15:27 +0200

    Replace IIII by IV succeed

--- src/DecimalToRoman.php
+++ src/DecimalToRoman.php
@@ -5,24 +5,24 @@ class DecimalToRoman{
     public static function decimalToRoman(int $number):string{
 
         $romanNumber = "";
-        if($number <= 3){
-            for ($i=0; $i < $number; $i++) { 
-                if($i <= 3){
-                    $romanNumber = $romanNumber."I";
-                }
-                
-            }
-            return $romanNumber;
-        }
-                
+        $quatre = "IV";
         
-        if($number == 4) return "IV";
-        if($number == 5) return "V";
-        if($number == 6) return "VI";
-        if($number == 7) return "VII";
-        if($number == 8) return "VIII";
-        if($number == 9) return "IX";
-        if($number == 10) return "X";
+        for ($i=0; $i < $number; $i++) { 
+            if($i <= 3){
+                $romanNumber = $romanNumber."I";
+            }
+            $romanNumber = preg_replace('/^(I){4}/', $quatre,$romanNumber);
+        }
+        //if($i == 4) $romanNumber = "IV";
+        if($i == 5) $romanNumber = "V";
+        if($i == 6) $romanNumber = "VI";
+        if($i == 7) $romanNumber = "VII";
+        if($i == 8) $romanNumber = "VIII";
+        if($i == 9) $romanNumber = "IX";
+        if($i == 10) $romanNumber = "X";
+        return $romanNumber;
+        
+        
 
         return $romanNumber;
     }
